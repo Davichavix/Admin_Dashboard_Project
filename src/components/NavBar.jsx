e8aa4c83975be7e892080ee05bf86d7aad26d2a9
David Chan <dchan902@gmail.com>
2022-06-07 22:22:58 -0600

    import react-icons and componenents into Navbar componenent

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { AiOutlineMenu } from 'react-icons/ai';
+import { FiShoppingCard } from 'react-icons/fi-shopping-cart';
+import { BsChatLeft } from 'react-icons/bs';
+import { RiNotification3Line } from 'react-icons/ri';
+import { MdKeyboardArrowDown } from 'react-icons/md';
+import { TooltipComponenet} from '@syncfusion/ej2-react-popups';
+import avatar from '../data/avatar.jpg'
+import { Cart, Chat, Notification, UserProfile} from '../components';
+import { useStateContext } from '../contexts/ContextProvider';
 
 const Navbar = () => {
   return (
