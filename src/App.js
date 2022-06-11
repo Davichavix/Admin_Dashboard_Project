0872b41998939f687caa091dae699705178a15ab
David Chan <dchan902@gmail.com>
2022-06-11 09:37:12 -0600

    add banner pattern to ecommerce page

--- src/App.js
+++ src/App.js
@@ -46,7 +46,6 @@ const App = () => {
             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
               <Navbar />
             </div>
-          </div>
 
           <div>
             <Routes>
@@ -75,7 +74,7 @@ const App = () => {
               <Route path="/stacked" element={<Stacked />} />  
             </Routes>
           </div>
-
+        </div>
         </div>
       </BrowserRouter>
     </div>
